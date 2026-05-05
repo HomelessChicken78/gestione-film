7810bf21d97f8c93735ca27631af1335fd197df8
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-05-05 09:46:58 +0200

    FEAT added more tables

--- ddl.sql
+++ ddl.sql
@@ -33,3 +33,33 @@ ADD CONSTRAINT `fk_film`
   REFERENCES `gestione-film`.`film` (`id_film`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
+
+CREATE TABLE `gestione-film`.`sala` (
+  `id_sala` INT NOT NULL,
+  `posti` INT NOT NULL,
+  `nome` VARCHAR(45) NOT NULL,
+  `citta` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`id_sala`));
+
+CREATE TABLE `gestione-film`.`proiezione` (
+  `id_proiezione` INT NOT NULL,
+  `id_film` INT NOT NULL,
+  `id_sala` INT NOT NULL,
+  `incasso` REAL NOT NULL,
+  `data_proiezione` INT NOT NULL,
+  PRIMARY KEY (`id_proiezione`));
+ALTER TABLE `gestione-film`.`proiezione` 
+ADD INDEX `fk_film_idx` (`id_film` ASC) VISIBLE,
+ADD INDEX `fk_sala_idx` (`id_sala` ASC) VISIBLE;
+;
+ALTER TABLE `gestione-film`.`proiezione` 
+ADD CONSTRAINT `fk_film`
+  FOREIGN KEY (`id_film`)
+  REFERENCES `gestione-film`.`film` (`id_film`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fk_sala`
+  FOREIGN KEY (`id_sala`)
+  REFERENCES `gestione-film`.`sala` (`id_sala`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
